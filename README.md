b74ec49e019b9f03da29e2b1b8c31e8d0652ee51
Khushraj Rathod <khushraj.rathod@gmail.com>
2020-10-20 00:16:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# hcload
+Upload files to Hackclub CDN from your CLI
+
+# Quickstart
+```
+npx hcload -f filename.txt
+```
+
+# Installation
+```
+npm install -g hcload
+```
+
+or 
+
+```
+yarn global add hcload
+```
+
+then run
+
+```
+hcload -f filename.txt
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const axios = require('axios')
+const { program } = require('commander')
+const { join, resolve, parse } = require('path')
+const app = require('express')()
+const ngrok = require('ngrok')
+
+program.version(require(join(__dirname, 'package')).version)
+program.requiredOption('-f, --file <file>', 'Path to file to upload')
+program.parse(process.argv)
+
+let fullPath = resolve('./' + program.file)
+let base = parse(fullPath).base
+
+app.get("/" + base, (_, res) => {
+    res.sendFile(fullPath)
+})
+
+const server = app.listen(0, async _ => {
+    console.log(`Listening at http://localhost:${server.address().port}`)
+    const url = await ngrok.connect(server.address().port)
+    axios
+        .post('https://cdn.hackclub.dev/api/new', [url + '/' + base])
+        .then(res => {
+            console.log(res.data[0])
+        })
+        .catch(error => {
+            console.error(error)
+        })
+})
+
